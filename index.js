0a9bd55917d692b171f06e5a9198b527cd2733ef
Jade <jade.besson@hotmail.fr>
2021-07-09 14:11:47 +0200

    a snail race

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Snail("Jade", 2, "green");
+  new Snail("Felipe", 2, "yellow");
+  new Snail("Kiki", 2, "blue");
+  new Snail("Manoel", 2, "purple");
+});
